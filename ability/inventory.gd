2bd9c01b8805fa70009005c0729b6a530fdf05c6
gregismotion <work@varigergo.hu>
2024-06-15 06:06:31 -0700

    feat: weight limitation

--- ability/inventory.gd
+++ ability/inventory.gd
@@ -7,8 +7,11 @@ signal changed(inventory : Inventory)
 @export var size : int = 12
 @export var weight_capacity : int = 25
 var weight : int : get = _get_weight
+var available_weight : int : get = _get_available_weight
 var stacks : Array[Stack]
  
+const stackgd = preload("res://inventory/stack.gd")
+
 func _ready():
 	stacks = []
 	stacks.resize(size)
@@ -21,6 +24,9 @@ func _get_weight():
 			result += stack.weight
 	return result
 
+func _get_available_weight():
+	return weight_capacity - weight
+
 func _is_space_enough() -> bool:
 	var total = 0
 	for stack in stacks:
@@ -28,6 +34,26 @@ func _is_space_enough() -> bool:
 			if not stack.is_empty():
 				total += 1
 	return total < size
+
+# [fits, didn't fit]
+func _get_stack_that_fits(source : Stack) -> Array[Stack]:
+	if _is_space_enough():
+		if source.weight <= available_weight:
+			return [source, null]
+		else:
+			var current := 0
+			var fitting : Stack = stackgd.new([])
+			while current < available_weight:
+				var popped := source.pop()
+				fitting.push(popped)
+				current += popped.weight
+			return [fitting, source]
+	return [null, source]
+
+func _get_stack_that_fits_and_drop(source : Stack) -> Stack:
+	var fitting := _get_stack_that_fits(source)
+	drop(fitting[1])
+	return fitting[0]
 
 func _get_first_free_index() -> int:
 	var i = 0
@@ -51,15 +77,19 @@ func _fill_available_stacks(new_stack : Stack) -> Stack:
 	return new_stack
 	
 func put(stack : Stack) -> Stack:
-	var remaining : Stack = _fill_available_stacks(stack)
-	if _is_space_enough() and not remaining.is_empty():
-		var target : int = _get_first_free_index()
-		stacks[target] = remaining
-		changed.emit(self)
-		return null
+	var remaining : Stack = _fill_available_stacks(_get_stack_that_fits_and_drop(stack))
+	
+	if remaining != null:
+		if not remaining.is_empty():
+			var target : int = _get_first_free_index()
+			stacks[target] = remaining
+			changed.emit(self)
+			return null
+		else:
+			changed.emit(self)
+			return null if remaining.is_empty() else remaining
 	else:
-		changed.emit(self)
-		return null if remaining.is_empty() else remaining
+		return null
 
 # FIXME: drop mechanism needs rework for stacks
 func drop(_stack : Stack):
@@ -80,19 +110,18 @@ func _ensure_size(target : int):
 			stacks.resize(target + 1)
 
 func move(stack : Stack, target: int):
+	var source_index := stacks.find(stack)
 	if target < size:
-		#_ensure_size(target) l# NOTE: not sure if needed
-		if target == stacks.find(stack): # if we were to move to the same place
+		if target == source_index: # if we were to move to the same place
 			return
-		elif stacks[target] == null: # there wasn't even a stack there
-			stacks[stacks.find(stack)] = null
-			stacks[target] = stack
-		elif stacks[target].is_empty(): # the stack there is empty
-			stacks[stacks.find(stack)] = null
+		elif stacks[target] == null or stacks[target].is_empty():
+			if source_index == -1:
+				stack = _get_stack_that_fits_and_drop(stack)
+			stacks[source_index] = null
 			stacks[target] = stack
 		else: # there's a stack there
 			var current_stack : Stack = stacks[target]
-			if stacks.find(stack) == -1: # stack not from inventory
+			if source_index == -1: # stack not from inventory
 				if current_stack.is_matching_stack(stack): # matching stack
 					stack = current_stack.transfer_all(stack)
 				target = _get_first_free_index()
@@ -104,6 +133,6 @@ func move(stack : Stack, target: int):
 				if current_stack.is_matching_stack(stack): # matching stack, we can transfer
 					current_stack.transfer_all(stack)
 				else: # not matching, we can replace	
-					stacks[stacks.find(stack)] = current_stack
+					stacks[source_index] = current_stack
 					stacks[target] = stack
 		changed.emit(self)
